fd4f6c947ebda2164f347336e57834c11b392c61
Karolina Ulloa <karolinaulloa@MacBook-Air-38.local>
2023-11-09 03:40:02 -0600

    separate html files

--- assets/js/cri.js
+++ assets/js/cri.js
@@ -11,3 +11,23 @@ tooltips.forEach(tooltip => {
     tooltipElement.textContent = tooltipText;
     tooltip.appendChild(tooltipElement);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+            const articleIds = ['precontent', 'acknowledgments', 'content-index', 'sec1', 'sec2', 'sec3', 'sec4', 'refs'];
+
+            articleIds.forEach(articleId => {
+                fetch(`contenido/${articleId}.html`)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.text();
+                    })
+                    .then(data => {
+                        document.getElementById(articleId).innerHTML = data;
+                    })
+                    .catch(error => {
+                        console.error(`Error fetching ${articleId}.html:`, error);
+                    });
+            });
+        });
