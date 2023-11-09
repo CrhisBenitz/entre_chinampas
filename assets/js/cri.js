16c9ff050d8ff60341f0aa741e8bf0acc87e9b4d
Karolina Ulloa <karolinaulloa@MacBook-Air-38.local>
2023-11-09 04:29:07 -0600

    separate html files

--- assets/js/cri.js
+++ assets/js/cri.js
@@ -19,19 +19,19 @@ document.addEventListener("DOMContentLoaded", function() {
                     .catch(error => {
                         console.error(`Error fetching ${articleId}.html:`, error);
                     });
+            });
 
-                    const tooltips = document.querySelectorAll('.tooltip');
+            const tooltips = document.querySelectorAll('.tooltip');
 
-                    // Loop through each tooltip
-                    tooltips.forEach(tooltip => {
-                        // Get the text for the tooltip from the 'data-tooltip' attribute
-                        const tooltipText = tooltip.getAttribute('data-tooltip');
+            // Loop through each tooltip
+            tooltips.forEach(tooltip => {
+                // Get the text for the tooltip from the 'data-tooltip' attribute
+                const tooltipText = tooltip.getAttribute('data-tooltip');
 
-                        // Create and append the tooltip text element
-                        const tooltipElement = document.createElement('span');
-                        tooltipElement.className = 'tooltiptext';
-                        tooltipElement.textContent = tooltipText;
-                        tooltip.appendChild(tooltipElement);
-                    });
+                // Create and append the tooltip text element
+                const tooltipElement = document.createElement('span');
+                tooltipElement.className = 'tooltiptext';
+                tooltipElement.textContent = tooltipText;
+                tooltip.appendChild(tooltipElement);
             });
         });
