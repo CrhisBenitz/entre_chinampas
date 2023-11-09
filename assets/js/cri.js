b3d422fbc105c981c42014e25c549d30688eb71d
Karolina Ulloa <karolinaulloa@MacBook-Air-38.local>
2023-11-09 04:42:31 -0600

    separate html files

--- assets/js/cri.js
+++ assets/js/cri.js
@@ -1,37 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Function to initialize tooltips
+function initializeTooltips() {
+    const tooltips = document.querySelectorAll('.tooltip');
 
+    tooltips.forEach(tooltip => {
+        const tooltipText = tooltip.getAttribute('data-tooltip');
+
+        const tooltipElement = document.createElement('span');
+        tooltipElement.className = 'tooltiptext';
+        tooltipElement.textContent = tooltipText;
+        tooltip.appendChild(tooltipElement);
+    });
+}
 });
 
+
 document.addEventListener("DOMContentLoaded", function() {
-            const articleIds = ['precontent', 'acknowledgments', 'content-index', 'sec1', 'sec2', 'sec3', 'sec4', 'refs'];
+    const articleIds = ['precontent', 'acknowledgments', 'content-index', 'sec1', 'sec2', 'sec3', 'sec4', 'refs'];
 
-            articleIds.forEach(articleId => {
-                fetch(`contenido/${articleId}.html`)
-                    .then(response => {
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok');
-                        }
-                        return response.text();
-                    })
-                    .then(data => {
-                        document.getElementById(articleId).innerHTML = data;
-                    })
-                    .catch(error => {
-                        console.error(`Error fetching ${articleId}.html:`, error);
-                    });
+    articleIds.forEach(articleId => {
+        fetch(`contenido/${articleId}.html`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById(articleId).innerHTML = data;
+            })
+            .then(() => {
+                // Initialize tooltips after each article's content is loaded
+                initializeTooltips();
+            })
+            .catch(error => {
+                console.error(`Error fetching ${articleId}.html:`, error);
             });
-
-            const tooltips = document.querySelectorAll('.tooltip');
-
-            // Loop through each tooltip
-            tooltips.forEach(tooltip => {
-                // Get the text for the tooltip from the 'data-tooltip' attribute
-                const tooltipText = tooltip.getAttribute('data-tooltip');
-
-                // Create and append the tooltip text element
-                const tooltipElement = document.createElement('span');
-                tooltipElement.className = 'tooltiptext';
-                tooltipElement.textContent = tooltipText;
-                tooltip.appendChild(tooltipElement);
-            });
-        });
+    });
