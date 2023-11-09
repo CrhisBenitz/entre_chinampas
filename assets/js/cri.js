e15efd42191f4c24eb32aeb70a3ccc8dd1ff3434
Karolina Ulloa <karolinaulloa@MacBook-Air-38.local>
2023-11-09 04:26:28 -0600

    separate html files

--- assets/js/cri.js
+++ assets/js/cri.js
@@ -1,17 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const tooltips = document.querySelectorAll('.tooltip');
 
-  // Loop through each tooltip
-  tooltips.forEach(tooltip => {
-      // Get the text for the tooltip from the 'data-tooltip' attribute
-      const tooltipText = tooltip.getAttribute('data-tooltip');
-
-      // Create and append the tooltip text element
-      const tooltipElement = document.createElement('span');
-      tooltipElement.className = 'tooltiptext';
-      tooltipElement.textContent = tooltipText;
-      tooltip.appendChild(tooltipElement);
-  });
 });
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -30,6 +18,20 @@ document.addEventListener("DOMContentLoaded", function() {
                     })
                     .catch(error => {
                         console.error(`Error fetching ${articleId}.html:`, error);
+                    });
+
+                    const tooltips = document.querySelectorAll('.tooltip');
+
+                    // Loop through each tooltip
+                    tooltips.forEach(tooltip => {
+                        // Get the text for the tooltip from the 'data-tooltip' attribute
+                        const tooltipText = tooltip.getAttribute('data-tooltip');
+
+                        // Create and append the tooltip text element
+                        const tooltipElement = document.createElement('span');
+                        tooltipElement.className = 'tooltiptext';
+                        tooltipElement.textContent = tooltipText;
+                        tooltip.appendChild(tooltipElement);
                     });
             });
         });
