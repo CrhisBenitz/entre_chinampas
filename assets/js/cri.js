c41016f4e78111a084b8fed494a9ddb5c315b92f
Karolina Ulloa <karolinaulloa@MacBook-Air-38.local>
2023-11-15 01:42:13 -0600

    sec4 commit

--- assets/js/cri.js
+++ assets/js/cri.js
@@ -1,3 +1,31 @@
+// Function to initialize figure numbers, references, and hyperlinks
+function initializeFiguresAndReferences() {
+  var figures = document.querySelectorAll(".figure");
+
+  figures.forEach(function(figure, index) {
+    var figureID = figure.getAttribute('id');
+    var figureNumber = index + 1;
+    var figureNumberElement = figure.querySelector(".figure-number");
+    figureNumberElement.innerHTML = "Figura " + figureNumber + ".";
+
+    // Create reference text
+    var referenceText = "figura" + figureNumber;
+
+    // Add reference to the text
+    var referenceSpan = document.getElementById("ref-" + figureID);
+    if (referenceSpan) {
+      // referenceSpan.textContent = referenceText;
+
+      // Create hyperlink to the figure with the reference text as its content
+      var figureLink = document.createElement('a');
+      figureLink.href = '#' + figureID;
+      figureLink.textContent = referenceText;
+      referenceSpan.appendChild(document.createTextNode(' ')); // Add space between the reference text and the link
+      referenceSpan.appendChild(figureLink);
+    }
+  });
+}
+
 // Function to initialize tooltips
 function initializeTooltips() {
     const tooltips = document.querySelectorAll('.tooltip');
@@ -12,11 +40,14 @@ function initializeTooltips() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const articleIds = ['precontent', 'acknowledgments', 'content-index', 'sec1', 'sec2', 'sec3', 'sec4', 'refs'];
 
+    // Array to store all fetch promises
+    const fetchPromises = [];
+
     articleIds.forEach(articleId => {
-        fetch(`contenido/${articleId}.html`)
+        const fetchPromise = fetch(`contenido/${articleId}.html`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -26,12 +57,20 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(data => {
                 document.getElementById(articleId).innerHTML = data;
             })
-            .then(() => {
-                // Initialize tooltips after each article's content is loaded
-                initializeTooltips();
-            })
             .catch(error => {
                 console.error(`Error fetching ${articleId}.html:`, error);
             });
+
+        fetchPromises.push(fetchPromise);
     });
+
+    // Wait for all fetch promises to complete
+    Promise.all(fetchPromises)
+        .then(() => {
+            // Initialize tooltips after all articles' content is loaded
+            initializeTooltips();
+
+            // Now call initializeFiguresAndReferences
+            initializeFiguresAndReferences();
+        });
 });
